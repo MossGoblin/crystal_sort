5a0feecc4baa1f00edf58a57a74c88e0b56e91c4
Angel Slavchev <aslavchev@roiti.com>
2021-06-18 17:29:05 +0300

    Built for publishing

--- tests/test_crystal_sort.py
+++ tests/test_crystal_sort.py
@@ -1,6 +1,7 @@
 from crystal_sort import __version__
 from crystal_sort import crystal_sort
 
+
 def test_version():
     assert __version__ == '0.1.0'
 
@@ -9,7 +10,17 @@ def test_sort_flag_false():
     ordered_bucket = crystal_sort.sort(bucket)
     assert ordered_bucket == sorted(bucket)
 
-def test_sort_flag_false():
+def test_sort_flag_true():
     bucket = [3, 5, 7, 1, 5, 9, 8, 4, 6, 2]
+    ordered_bucket = crystal_sort.sort(bucket, True)
+    assert ordered_bucket == sorted(bucket)
+
+def test_sort_flag_floating_false():
+    bucket = [3.0, 5.1, 7.2, 1.3, 5.4, 9.5, 8.6, 4.7, 6.8, 2.9]
+    ordered_bucket = crystal_sort.sort(bucket)
+    assert ordered_bucket == sorted(bucket)
+
+def test_sort_flag_floating_true():
+    bucket = [3.0, 5.1, 7.2, 1.3, 5.4, 9.5, 8.6, 4.7, 6.8, 2.9]
     ordered_bucket = crystal_sort.sort(bucket, True)
     assert ordered_bucket == sorted(bucket)
